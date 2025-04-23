946a18fb1837a3a06d52ef64ad57f1b22ca96d03
Yang Bai <yang.baixjtlu@gmail.com>
2025-04-23 22:31:01 +0800

    Add WebSecurityConfig

--- src/main/java/com/ecommerce/controller/AuthController.java
+++ src/main/java/com/ecommerce/controller/AuthController.java
@@ -18,6 +18,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,7 +64,7 @@ public class AuthController {
         String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
 
         List<String> roles = userDetails.getAuthorities().stream()
-                .map(item -> item.getAuthority())
+                .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
 
         UserInfoResponse response = new UserInfoResponse(userDetails.getId(),
