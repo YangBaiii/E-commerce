dba4852436155c76b3d46df734f66ae7acc9965e
Yang Bai <yang.baixjtlu@gmail.com>
2025-06-11 00:12:45 +0800

    Add product basics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Products from "./components/Products";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div
-          className='flex items-center justify-center bg-gray-800 text-3xl'>
-          Welcome
-      </div>
+      <>
+          <Products />
+      </>
   )
 }
 
